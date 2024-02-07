97dfbc6ea94ada475f5886781533f88971bcc561
Tom Pierce <tompierce4@gmail.com>
2024-02-07 18:03:51 +0000

    typo in logout redirect url

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,6 +1,6 @@
 const logout = () => {
   try {
-    window.open(`${import.meta.env.VITE_API_URL}/api/auth/logout`, "_self");
+    window.open(`${import.meta.env.VITE_API_URL}api/auth/logout`, "_self");
   } catch (error) {
     console.error("Error logging out:", error);
   }
